f576c55e239a92dc856df64dc17eb4728d1873c3
3bdoelnaggar <3bdoelnaggar@gmail.com>
2020-11-14 03:06:17 +0200

    add eboxy library and create popular_people_list_activity

---
+++ app/src/main/java/com/elnaggar/ibtikartask/features/popularPeopleList/PopularPeopleListActivity.kt
@@ -0,0 +1,12 @@
+package com.elnaggar.ibtikartask.features.popularPeopleList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.elnaggar.ibtikartask.R
+
+class PopularPeopleListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.popular_people_list_activity)
+    }
+}
--- data/src/main/java/com/elnaggar/data/TmdbService.kt
+++ data/src/main/java/com/elnaggar/data/TmdbService.kt
@@ -5,9 +5,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 
-const val api_key = "e5bacc18a0877161086a694a97bd477b"
+const val API_KEY = "e5bacc18a0877161086a694a97bd477b"
+const val IMAGE_BASE_URL="https://image.tmdb.org/t/p/w200"
 
 interface TmdbService {
-    @GET("person/popular?api_key=$api_key")
+    @GET("person/popular?api_key=$API_KEY")
     suspend fun getPopularPersons(@Query("page") page: Int): PagedApiResponse<Person>
 }
